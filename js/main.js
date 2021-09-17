e1c5c5d826028ea24db047faed1ad95bf5d9a3fd
FrankLaurora <francesco.laurora9@gmail.com>
2021-09-17 17:16:27 +0200

    Add click event to browse photos

--- js/main.js
+++ js/main.js
@@ -6,6 +6,28 @@ const app = new Vue({
             "https://placebear.com/600/400.jpg",
             "https://placebear.com/900/600.jpg",
             "https://placebear.com/1200/800.jpg"
-        ]
+        ],
+
+        photoIndex: 0,
+    },
+
+    methods: {
+        nextPhoto: function() {
+            // return this.photoIndex++;
+            if(this.photoIndex == this.photos.length - 1) {
+                return this.photoIndex = 0;
+            } else {
+                return this.photoIndex++;
+            }
+        },
+
+        prevPhoto: function() {
+            // return this.photoIndex--;
+            if(this.photoIndex == 0) {
+                return this.photoIndex = this.photos.length - 1;
+            } else {
+                return this.photoIndex--;
+            }
+        },
     }
 }); 
