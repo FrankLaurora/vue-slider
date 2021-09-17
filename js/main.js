c8bace1a82ffd35da7e3f3c885f08e94dd9e9533
FrankLaurora <francesco.laurora9@gmail.com>
2021-09-17 16:52:48 +0200

    Init project

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+    el: "#root",
+
+    data: {
+        photos: [
+            "https://placebear.com/600/400.jpg",
+            "https://placebear.com/900/600.jpg",
+            "https://placebear.com/1200/800.jpg"
+        ]
+    }
+}); 
